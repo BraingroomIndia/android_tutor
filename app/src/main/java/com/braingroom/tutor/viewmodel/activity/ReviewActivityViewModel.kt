6747fba3721372e2009f94bf5b7074569a1082e8
Vikas Godara <viki.godara@gmail.com>
2018-01-05 15:56:00 +0530

    Review Loading Item added

--- app/src/main/java/com/braingroom/tutor/viewmodel/activity/ReviewActivityViewModel.kt
+++ app/src/main/java/com/braingroom/tutor/viewmodel/activity/ReviewActivityViewModel.kt
@@ -9,11 +9,12 @@ import com.braingroom.tutor.utils.isEmpty
 import com.braingroom.tutor.view.adapters.EqualSpacingItemDecoration
 import com.braingroom.tutor.view.adapters.ViewProvider
 import com.braingroom.tutor.viewmodel.ViewModel
-import com.braingroom.tutor.viewmodel.item.NotifyDataSetChanged
-import com.braingroom.tutor.viewmodel.item.ReviewItemViewModel
+import com.braingroom.tutor.viewmodel.item.*
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import java.util.*
 
-/**
+/*
  * Created by ashketchup on 22/12/17.
  */
 class ReviewActivityViewModel : ViewModel() {
@@ -23,6 +24,7 @@ class ReviewActivityViewModel : ViewModel() {
             override fun getView(vm: ViewModel?): Int {
                 return when (vm) {
                     is ReviewItemViewModel -> R.layout.item_review
+                    is LoadingViewModel -> R.layout.item_loading_media
                     null -> throw NullPointerException()
                     else -> throw NoSuchFieldError()
                 }
@@ -32,18 +34,31 @@ class ReviewActivityViewModel : ViewModel() {
     val decor: RecyclerView.ItemDecoration = EqualSpacingItemDecoration(convertDpToPixel(11).toInt(), VERTICAL)
 
     init {
-        FieldUtils.toObservable(callAgain).filter({ _ -> pageNumber > -1 && !paginationInProgress }).subscribe({
+        FieldUtils.toObservable(callAgain).filter({ _ -> pageNumber > -1 && !paginationInProgress }).doOnSubscribe { disposable ->
+            compositeDisposable.add(disposable)
+            (0..5).forEach { _ -> item.onNext(LoadingViewModel()) }
+            item.onNext(NotifyDataSetChanged())
+
+        }.subscribe({
             paginationInProgress = true
-            apiService.getReview(pageNumber).observeOn(AndroidSchedulers.mainThread()).subscribe { t ->
-                t.data.forEach { x ->
-                    if(isEmpty(x.classId))
-                        item.onNext(ReviewItemViewModel(x.reviewMessage, x.rating, x.firstName,"Vendor Review"))
-                    else
-                        item.onNext(ReviewItemViewModel(x.reviewMessage,x.rating,x.firstName,x.classTopic))
-                    item.onNext(NotifyDataSetChanged())
+            apiService.getReview(pageNumber).map { resp ->
+                if (resp.resCode) {
+                    resp.data.map { snippet ->
+                        if (isEmpty(snippet.classId))
+                            ReviewItemViewModel(snippet.reviewMessage, snippet.rating, snippet.firstName, "Vendor Review")
+                        else
+                            ReviewItemViewModel(snippet.reviewMessage, snippet.rating, snippet.firstName, snippet.classTopic)
+                    }
+                } else (pageNumber == 1)
+                Collections.singletonList(EmptyItemViewModel("", R.drawable.ic_no_post_64dp, "No Review Found"))
+            }.subscribe { viewModelList ->
+                item.onNext(RemoveLoadingViewModel())
+                viewModelList.forEach { viewModel ->
+                    item.onNext(viewModel)
                 }
+                item.onNext(NotifyDataSetChanged())
                 paginationInProgress = false
-                if (t == null || t.data == null || t.data.isEmpty())
+                if (viewModelList[0] is EmptyItemViewModel)
                     pageNumber = -1
                 else pageNumber++
             }
